3502e3b55c7f0ebd17943583ec0c5e09f464e391
Jan Dočkal <dockal07@gmail.com>
2018-11-11 16:13:31 +0100

    Added module that enables bluetooth

--- src/Core/Bluetooth/requestBluetoothPermisions.ts
+++ src/Core/Bluetooth/requestBluetoothPermisions.ts
@@ -2,9 +2,18 @@ import {
 	Platform,
 	PermissionsAndroid
 } from 'react-native';
+import { BluetoothStatus } from 'react-native-bluetooth-status';
 
 export const requestBluetoothPermisions = async () => {
 	if (Platform.OS === 'android' && Platform.Version >= 23) {
+		const enabled = await BluetoothStatus.state();
+		if (!enabled) {
+			try {
+				BluetoothStatus.enable(true);
+			} catch(e) {
+				console.log(e);
+			}
+		}
 		const granted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION);
 		if (!granted) {
 			const grantedAfterRequest = await PermissionsAndroid.requestPermission(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION);
