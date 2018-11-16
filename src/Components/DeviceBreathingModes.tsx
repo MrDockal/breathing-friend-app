ec924593cbd77b8bafc4f534c8a210f15af782a2
Jan Dočkal <dockal07@gmail.com>
2018-11-16 17:48:16 +0100

    Format minutes & seconds to human readable

--- src/Components/DeviceBreathingModes.tsx
+++ src/Components/DeviceBreathingModes.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { Text } from 'react-native';
-import { ListItemProps, Divider, Button as ButtonRN } from 'react-native-elements';
+import { ListItemProps, Divider } from 'react-native-elements';
 import { themeSchema } from '../Core/ThemeSchema/themeSchema';
-import { Button } from './Button';
 import { AppList } from './AppList';
 import { Device } from '../Core/Entities/Device';
 import { BreathingMode, DeviceSavedBreathingMode, BreathingSpeed } from '../Core/Entities/BreathingMode';
@@ -19,6 +18,27 @@ export interface OwnProps {
 
 export class DeviceBreathingModes extends React.Component<OwnProps> {
 	public render() {
+		const breathingModes = this.prepareBreathingModes();
+		return (
+			<React.Fragment>
+				<Text style={{color: themeSchema.color.fontColor, fontSize: themeSchema.fontSize.h1}}>Lorem ipsum dole more</Text>
+				<Text style={{color: themeSchema.color.fontColor, fontSize: themeSchema.fontSize.h2}}>Lorem ipsum dole more</Text>
+				<Text style={{color: themeSchema.color.fontColor, fontSize: themeSchema.fontSize.normal}}>Lorem ipsum dole more</Text>
+				<AppList list={breathingModes.activeModesList}/>
+				<Divider/>
+				<AppList list={breathingModes.availableModesList}/>
+			</React.Fragment>
+		)
+	}
+
+	private convertMinutesToText(minutes: number) {
+		const minutesFixed = parseFloat(minutes.toFixed(2));
+		const justMinutes = Math.trunc(minutesFixed);
+		const seconds = Math.round((minutesFixed - justMinutes) * 60);
+		return `${justMinutes} minutes ${(seconds > 0) ? `${seconds} seconds` : ''}`;
+	}
+	
+	private prepareBreathingModes() {
 		const activeModesUids = this.props.activeDevice.breathingModes.map((mode: DeviceSavedBreathingMode) => mode.uid);
 		const activeModes = this.props.breathingModes
 			.filter((mode: BreathingMode) => activeModesUids.indexOf(mode.uid) >= 0)
@@ -33,7 +53,7 @@ export class DeviceBreathingModes extends React.Component<OwnProps> {
 		});
 		const activeModesList = activeModes.map((mode: BreathingModeWithActiveSpeed, index: number): ListItemProps => ({
 			title: mode.name,
-			subtitle: `${mode.speed[mode.activeSpeed].duration} minut`,
+			subtitle: this.convertMinutesToText(mode.speed[mode.activeSpeed].duration),
 			rightTitle: `Pozice ${index + 1}`,
 			rightTitleStyle: { paddingTop: 25 },
 			onPress: () => this.props.goToModeDetail(mode, 'edit', mode.activeSpeed),
@@ -43,15 +63,9 @@ export class DeviceBreathingModes extends React.Component<OwnProps> {
 			subtitle: `${mode.speed.normal.duration} minut`,
 			onPress: () => this.props.goToModeDetail(mode, 'add'),
 		}));
-		return (
-			<React.Fragment>
-				<Text style={{color: themeSchema.color.fontColor, fontSize: themeSchema.fontSize.h1}}>Lorem ipsum dole more</Text>
-				<Text style={{color: themeSchema.color.fontColor, fontSize: themeSchema.fontSize.h2}}>Lorem ipsum dole more</Text>
-				<Text style={{color: themeSchema.color.fontColor, fontSize: themeSchema.fontSize.normal}}>Lorem ipsum dole more</Text>
-				<AppList list={activeModesList}/>
-				<Divider/>
-				<AppList list={availableModesList}/>
-			</React.Fragment>
-		)
+		return {
+			activeModesList,
+			availableModesList
+		}
 	}
 }
